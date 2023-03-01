29737ecc2f3cb43ee2133671e2ad7346db8ca54b
Bar Barina <itsbarbarina@gmail.com>
2023-03-01 15:23:29 +0200

    handled mail read

--- apps/email/cmps/EmailPreview.js
+++ apps/email/cmps/EmailPreview.js
@@ -2,7 +2,7 @@ export default {
     name: 'emailPreview',
     props: ['email'],
     template: `
-    <div class="email-preview">
+    <div class="email-preview" :class="{read: email.isRead}">
         <p>{{ email.from}}</p>
             <p>{{ email.subject}}</p>
             <p>{{ email.sentAt}}</p>
