a751a4a3345ca500584f6398e2450d988a03e701
Bar Barina <itsbarbarina@gmail.com>
2023-03-04 21:05:03 +0200

    test for github

--- views/AboutUs.js
+++ views/AboutUs.js
@@ -1,15 +1,12 @@
 export default {
 	template: `
         <section class="about-page">
-            <!-- <h1>Our Team is built different.</h1> -->
             <div class="our-cards">
-                <img src="assets/img/dorImg.jpg">
                 <h2>Dor Tayari</h2>
                 <p>Web developer and a genius.</p>
                 <p>DorTayari@gmail.com</p>
             </div>
             <div class="our-cards">
-            <img src="assets/img/barImg.jpg" class="bar">
                 <h2>Bar Barina</h2>
                 <p>Web developer and a genius.</p>
                 <p>BarBarina@gmail.com</p>
