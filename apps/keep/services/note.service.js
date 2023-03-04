91cdf3066365c55e0937a3244f242abde7109d81
DorTeri <dortayari@gmail.com>
2023-03-05 00:04:58 +0200

    Test

--- apps/keep/services/note.service.js
+++ apps/keep/services/note.service.js
@@ -35,7 +35,7 @@ const notes = [
     {
         id: 'n102',
         type: 'NoteImg',
-        isPinned: false,
+        isPinned: true,
         info: {
             url: 'https://images.unsplash.com/photo-1512621776951-a57141f2eefd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
             title: 'Many colors',
--- views/AboutUs.js
+++ views/AboutUs.js
@@ -1,18 +1,11 @@
 export default {
-	template: `
-        <section class="about-page">
-            <div class="our-cards">
-                <div>
-                <h2>Dor Tayari</h2>
-                <p>Web developer and a genius.</p>
-                <p>DorTayari@gmail.com</p>
-                </div>
-            </div>
-            <div class="our-cards">
-                <h2>Bar Barina</h2>
-                <p>Web developer and a genius.</p>
-                <p>BarBarina@gmail.com</p>
-            </div>
-        </section>
+    template: `
+       <section class="about-page">
+        
+       </section>
     `
 }
+
+    // < h2 > Bar Barina</ >
+    //             <p>Web developer and a genius.</p>
+    //             <p>BarBarina@gmail.com</p>
