72312f3fdd8b9e0feb6ab61dd62c6630eaf65319
DorTeri <dortayari@gmail.com>
2023-03-05 00:33:53 +0200

    About

--- views/AboutUs.js
+++ views/AboutUs.js
@@ -1,7 +1,33 @@
 export default {
     template: `
        <section class="about-page">
-        
+       <h2>Our Team</h2>
+       <div class="our-team">
+            <div class="card">
+                <img src="assets/img/dor-img.jpg">
+                <div class="icons">
+                <i class="fa-brands fa-linkedin"></i>
+                <i class="fa-brands fa-facebook"></i>
+                <i class="fa-brands fa-twitter"></i>
+                <i class="fa-solid fa-envelope"></i>
+                </div>
+            </div>
+            <div class="card">
+                <img src="assets/img/bar-img.jpg">
+                <div class="icons">
+                <i class="fa-brands fa-linkedin"></i>
+                <i class="fa-brands fa-facebook"></i>
+                <i class="fa-brands fa-twitter"></i>
+                <i class="fa-solid fa-envelope"></i>
+                 </div>
+              </div>
+            </div>
+        </div>
+        <div class="our-services">
+            <h2><i class="fa-solid fa-gear fa-spin"></i>
+            Our services
+            <i class="fa-solid fa-gear fa-spin"></i></h2>
+        </div>
        </section>
     `
 }
