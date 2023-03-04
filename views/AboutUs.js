029a990c849460e095e848cb0dbc8acde7b9cba9
DorTeri <dortayari@gmail.com>
2023-03-04 21:38:00 +0200

    Final

--- views/AboutUs.js
+++ views/AboutUs.js
@@ -2,9 +2,11 @@ export default {
 	template: `
         <section class="about-page">
             <div class="our-cards">
+                <div>
                 <h2>Dor Tayari</h2>
                 <p>Web developer and a genius.</p>
                 <p>DorTayari@gmail.com</p>
+                </div>
             </div>
             <div class="our-cards">
                 <h2>Bar Barina</h2>
